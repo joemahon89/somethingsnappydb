4e49cc8fbdc2f5164a06946a9d6fc302310dda0c
Adriana Toutoudaki <adriana.tou@gmail.com>
2020-02-26 13:03:12 +0000

    added data and bin folders

---
+++ bin/visualise_db.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+#
+#
+#
+#
+# Adriana Toutoudaki and Joseph Mahon (February 2020)
+# contact: adriana.toutoudaki@addenbrookes.nhs.uk
+
+
+import pandas as pd
+
+data = pd.read_csv("/Users/Addy/Projects/somethingsnappydb/data/BRCA1_variants.txt",'\t')
+
+print (data)
