3d32ef045cf89b323e21b3a1882827c6ef427080
jmahon <joseph.mahon@nhs.net>
2020-02-27 16:41:31 +0000

    Commit of variant at pos pages

--- somethingsnappydb/somethingsnappydb_app/urls.py
+++ somethingsnappydb/somethingsnappydb_app/urls.py
@@ -5,6 +5,11 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('patient',views.patient, name='patient'),
     path('variant',views.variant, name='variant'),
-    path('position_variants',views.position_variants, name='position_variants'),
+    path('results/',views.search, name='search'),
+    path('position_variants/<str:chromosome>/<str:position>/',
+			    	views.position_variants, 
+			    	name='position_variants'),
 ]
+
+
 
--- somethingsnappydb/somethingsnappydb_app/views.py
+++ somethingsnappydb/somethingsnappydb_app/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import (Patient,
+                    SampleType,
+                    SomaticInformation,
+                    Sample,
+                    Sequencer,
+                    Analysis,
+                    Variant,
+                    RefGenome,
+                    AnalysisVariant,
+                    Interpretation,
+                    Criteria,
+                    InterpretationCriteria,)
+
 
 # Create your views here.
+
+# Search related Views
+def search(request):
+    query = request.GET['query']
+    if "-" in query:
+        chromosome = int(query.split("-")[0])
+        position = int(query.split("-")[1])
+        return redirect('position_variants', 
+                        chromosome=chromosome,
+                        position=position)
+    else:
+        pass
+
 
 def home(request):
     #return HttpResponse("Welcome to the Something Snappy Database")
@@ -14,6 +40,30 @@ def patient(request):
 def variant(request):
     return render (request,'somethingsnappydb_app/variant.html')
 
-def position_variants(request):
-    return render (request,'somethingsnappydb_app/position_variants.html')
-    
+
+# Position Variants Related Views
+
+
+def position_variants(request, chromosome, position):
+    variants_at_pos = AnalysisVariant.objects.select_related(
+                                    'genome_id').all().select_related(
+                                    'variant_id').all().filter(
+                         variant_id__pos=position,
+                         variant_id__chrom=chromosome)
+    variants_at_pos = variants_at_pos.prefetch_related("interpretation_set").all()
+    #for i in variants_at_pos:
+        #print(i.variant_id.raw_g)
+        #print(vars(i))
+        #print(i.genome_id.name)
+        #print(i.interpretation_set)
+        #print("#####")
+        #print(vars(i.interpretation_set.all()[0].pathogenicity))
+        #print(i.interpretation_set.all()[0].pathogenicity)
+    context = {"variants" : variants_at_pos,
+                "position": position,
+                "chromosome": chromosome,
+                }
+    return render(request,
+                'somethingsnappydb_app/position_variants.html',
+                context)
+
